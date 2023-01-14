ef16857a79e5fa164072c34080e642a16f7b7fcb
FriedaWu <90744868+FriedaWu@users.noreply.github.com>
2023-01-14 16:54:21 +0800

    Update main.go

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/NJUPT-ISL/Yoda-Scheduler/pkg/register"
+	"github.com/chenjiale12345/scheduler/pkg/register"
 	"k8s.io/component-base/logs"
 	"math/rand"
 	"os"
